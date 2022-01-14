3fb0bc142f0eeb9994a971d8d2f8459541d64b89
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-14 08:45:45 -0500

    checked for --exclusive

--- dev.bash
+++ dev.bash
@@ -1,6 +1,11 @@
+echo old
+exit 1
+
+# << checked for --exclusive >>
+
 ~/app/make.bash
 cwd=`pwd`
-prep "." "$" query.ohm query.glue --stop=1 --support=${cwd}/support.js <names.md >p1
+prep "." "$" query.ohm query.glue --stop=1 --support=${cwd}/support.js --exclusive <names.md >p1
 chmod a+x p1
 ./p1 >out
 
